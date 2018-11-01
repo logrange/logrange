0d3d76692b8c542b06fcf204e4e968a30a8b6be2
Dmitry Spasibenko <dspasibenko@gmail.com>
2018-11-01 16:07:06 -0700

    backup before tests

--- pkg/records/journal/jrnlfs/controller.go
+++ pkg/records/journal/jrnlfs/controller.go
@@ -1,11 +1,34 @@
 package jrnlfs
 
+import (
+	"github.com/logrange/logrange/pkg/records/journal"
+)
+
 type (
 	// Controller struct allows to scan provided folder on a file-system,
 	// collects information about possible logrange database and manages the
 	// found journals via its interface
 	//
-	// The Controller implements records.Controller and records.ChunksController
+	// The Controller implements journal.Controller and journal.ChunksController
 	Controller struct {
 	}
 )
+
+func NewController() *Controller {
+	c := new(Controller)
+	return c
+}
+
+// ----------------------- journal.Controller -------------------------------
+func (c *Controller) GetOrCreate(ctx context.Context, jname string) (Journal, error) {
+
+}
+
+// --------------------- journal.ChunksController ----------------------------
+func (c *Controller) GetChunks(jid uint64) chunk.Chunks {
+
+}
+
+func (c *Controller) NewChunk(ctx context.Context, jid uint64) (chunk.Chunk, error) {
+
+}
