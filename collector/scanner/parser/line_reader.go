ca7a45e219ee1fb3581df7f7b9981da7a5eaac79
kbabushkin <kbabushkin@hotmail.com>
2019-02-27 11:55:38 -0800

    update copyright

--- collector/scanner/parser/line_reader.go
+++ collector/scanner/parser/line_reader.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The logrange Authors
+// Copyright 2018-2019 The logrange Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this f except in compliance with the License.
